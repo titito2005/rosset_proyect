694dc2ede12d28277283834f08c5c56d3983b35f
Diego-Barquero-Quesada <diego.barqueroquesada@ucr.ac.cr>
2021-10-04 18:41:03 -0600

    creacion de formula para guardar los inputs de los datos de los productos

--- Rosset_Ionic/src/app/save-products/save-products.page.ts
+++ Rosset_Ionic/src/app/save-products/save-products.page.ts
@@ -1,4 +1,12 @@
+
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import {LoadingController, ToastController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-save-products',
@@ -10,6 +18,16 @@ export class SaveProductsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let cantidad = form.value.cantidad;
+    console.log(cantidad);
+    /*
+    if (!form.valid) {
+      return;
+    }
+    */
   }
 
 }
