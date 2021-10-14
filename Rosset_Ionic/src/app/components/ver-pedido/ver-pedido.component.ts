f709199e7f451f24ba546557e66931e38f7d3b35
Luis <luis.rojascarrillo@ucr.ac.cr>
2021-10-13 16:08:40 -1200

    Agregando componente ver pedido

---
+++ Rosset_Ionic/src/app/components/ver-pedido/ver-pedido.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-pedido',
+  templateUrl: './ver-pedido.component.html',
+  styleUrls: ['./ver-pedido.component.scss'],
+})
+export class VerPedidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
