28ad5408585c5f867f83830f73e693ac1c7f4f15
Luis <luis.rojascarrillo@ucr.ac.cr>
2021-10-24 12:54:18 -1200

    Agregando conteo de productos por proceso de producción.

--- Rosset_Ionic/src/app/pages/produccion/produccion.page.ts
+++ Rosset_Ionic/src/app/pages/produccion/produccion.page.ts
@@ -23,32 +23,99 @@ export class ProduccionPage implements OnInit {
   datos: boolean = false;
   listaProductos: Producto[] = [];
 
+  listaCorte: Producto[] = [];
+  listaEstampado: Producto[] = [];
+  listaPlanchado: Producto[] = [];
+  listaCosido: Producto[] = [];
+  listaEmpacado: Producto[] = [];
+  listaEntregado: Producto [] = [];
+
   async ngOnInit() {
     const querySnapshot = await this.produccionService.getProductos();
     var contador = 0;
     querySnapshot.forEach((doc) => {
-      let producto: Producto = {
-        camisa: doc.get("Camisa"),
+      if(doc.get("Camisa"))
+      {
+        let producto_camisa: Producto = {
+          camisa: doc.get("Camisa"),
+          cantidad: doc.get("Cantidad"),
+          cuelloCam: doc.get("CuelloCam"),
+          detalles: doc.get("Detalles"),
+          estadoCam: doc.get("EstadoCam"),
+          estampado: doc.get("Estampado"),
+          mangaCam: doc.get("MangaCam"),
+          nombre: doc.get("Nombre"),
+          pedido: doc.get("Pedido"),
+          precio: doc.get("Precio"),
+          tallaCam: doc.get("TallaCam"),
+          tela: doc.get("Tela"),
+          pantalon: false,
+          estadoPant: 0,
+          tallaPant: "",
+          largoPant: 0,
+        };
+        this.estadoProduccion(producto_camisa);
+      }
+
+      if(doc.get("Pantalon"))
+      {
+        let producto_pantalon: Producto = {
         cantidad: doc.get("Cantidad"),
-        cuelloCam: doc.get("CuelloCam"),
         detalles: doc.get("Detalles"),
-        estadoCam: doc.get("EstadoCam"),
         estadoPant: doc.get("EstadoPant"),
         estampado: doc.get("Estampado"),
         largoPant: doc.get("LargoPant"),
-        mangaCam: doc.get("MangaCam"),
         nombre: doc.get("Nombre"),
         pantalon: doc.get("Pantalon"),
         pedido: doc.get("Pedido"),
         precio: doc.get("Precio"),
-        tallaCam: doc.get("TallaCam"),
         tallaPant: doc.get("TallaPant"),
         tela: doc.get("Tela"),
-      };
-      this.listaProductos[contador]=producto;
-      contador++;
+        camisa: false,
+        estadoCam: 0,
+        mangaCam: 0,
+        cuelloCam: 0,
+        tallaCam: "",
+        };
+        this.estadoProduccion(producto_pantalon);
+      }
     });
     this.datos = true;
+  }
+
+  estadoProduccion(producto: Producto)
+  {
+    let estado: number = 0;
+    if(producto.camisa)
+    {
+      estado = producto.estadoCam;
+    }else{
+      estado = producto.estadoPant;
+    }
+
+    switch(estado)
+    {
+      case 0:
+        this.listaCorte.push(producto);
+      break;
+      case 1:
+        this.listaEstampado.push(producto);
+      break;
+      case 2:
+        this.listaPlanchado.push(producto);
+      break;
+      case 3:
+        this.listaCosido.push(producto);
+      break;
+      case 4:
+        this.listaEmpacado.push(producto);
+      break;
+      case 5:
+        this.listaEntregado.push(producto);
+      break;
+      default:
+      break;
+    }
   }
 }
 
